d43b4361022ec7a5f42382bf864dd36fc019bd2c
Cheng-K <adamkei2612@gmail.com>
2023-02-07 21:32:33 +0800

    Rewrite bootstrap with react-bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { MdSettings, MdMenu } from "react-icons/md";
 
 function Header() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-2 d-flex justify-content-start align-items-center">
+    <Container fluid>
+      <Row>
+        <Col
+          className="d-flex justify-content-start align-items-center p-2"
+          xs={2}
+        >
           <MdMenu size="2rem" />
-        </div>
-        <div className="col">
+        </Col>
+        <Col className="col p-2">
           <h2 className="text-center">Stay focused</h2>
-        </div>
-        <div className="col-2 d-flex justify-content-end align-items-center">
+        </Col>
+        <Col
+          className="d-flex justify-content-end align-items-center p-2"
+          xs={2}
+        >
           <MdSettings size="2rem" />
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
