6f536e1eda325a6cfe9c2d86d3fda85e1ac9a767
Cheng-K <adamkei2612@gmail.com>
2023-02-14 14:50:55 +0800

    Added tooltip display for tasks panel and settings icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,20 +11,25 @@ function Header({ isWorkingSession }) {
       <Row className="p-3">
         <Col className="d-flex justify-content-start align-items-center" xs={2}>
           <TaskButton
-            className="control-button-small"
+            className="control-button-small show-text"
             iconStyle="icon-small icon-black"
           />
+          <span>Show Tasks</span>
         </Col>
         <Col className="col">
           <h1 className="text-center fs-2 fw-bold">
             {isWorkingSession ? "Stay focused" : "Take a break"}
           </h1>
         </Col>
-        <Col className="d-flex justify-content-end align-items-center" xs={2}>
+        <Col
+          className="d-flex justify-content-start align-items-center flex-row-reverse"
+          xs={2}
+        >
           <SettingsButton
-            className="control-button-small"
+            className="control-button-small show-text"
             iconStyle="icon-small icon-black"
           />
+          <span className="text-end">Settings</span>
         </Col>
       </Row>
     </Container>
