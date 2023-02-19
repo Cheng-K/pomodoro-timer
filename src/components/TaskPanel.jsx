f3bdf1ac6ad6ae759328fa45d08954e5f6176e07
Cheng-K <adamkei2612@gmail.com>
2023-02-19 22:56:42 +0800

    Updated the edit functionality in task panel
    Changes :
    1. Each task can be individually toggled to edit in edit mode
    2. Each task is an individual form in edit mode
    3. Value edited in each task will not be saved unless explicitly click done button
    4. Toggling task to done will stroke the task
    5. Updates to the icon of exiting edit mode

--- src/components/TaskPanel.jsx
+++ src/components/TaskPanel.jsx
@@ -1,89 +1,87 @@
 import dayjs from "dayjs";
 import React, { useReducer, useState } from "react";
-import Form from "react-bootstrap/Form";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import AddTaskModal from "./AddTaskModal";
 import AddButton from "./task_panel/AddButton";
 import CloseButton from "./task_panel/CloseButton";
 import DeleteAllButton from "./task_panel/DeleteAllButton";
-import BackButton from "./task_panel/DoneButton";
 import EditButton from "./task_panel/EditButton";
 import Task from "./task_panel/Task";
 
 function TaskPanel({ show, handleClose, ...props }) {
-  const [isEditing, toggleEditing] = useReducer(
-    (currentState) => !currentState,
-    false
-  );
+  const [inEditMode, setInEditMode] = useState(false);
   const [tasksList, updateTaskList] = useReducer(
     (task, request) => {
       if (request.type === "POST") {
         return [...task, request.task];
       } else if (request.type === "PUT") {
         return task.map((t) => {
-          if (t.key === request.task.key) {
+          if (t.id === request.task.id) {
             return request.task;
           }
           return t;
         });
       } else if (request.type === "DELETE") {
         return task.filter((t) => {
-          t.key !== request.task.key;
+          t.id !== request.task.id;
         });
       }
     },
-    [{ key: 1, title: "Programming", dueDateTime: dayjs() }]
+    [{ id: 1, title: "Programming", dueDateTime: dayjs() }]
   );
   const [addModalShowing, setAddModalShowing] = useState(false);
-  const [editValidated, setEditValidated] = useState(false);
-  const onEditFinish = (event) => {
-    const form = event.currentTarget;
-    event.preventDefault();
-    event.stopPropagation();
-    setEditValidated(true);
-    if (form.checkValidity() !== false) {
-      setEditValidated(false);
-      toggleEditing();
-    }
-  };
+  const allTasks = tasksList.map((task) => {
+    let dueDate = task.dueDateTime.format("YYYY-MM-DD");
+    let dueTime = task.dueDateTime.format("HH:mm");
+    if (dueDate === "Invalid Date") dueDate = "";
+    if (dueTime === "Invalid Date") dueTime = "";
+    return (
+      <Task
+        key={task.id}
+        id={task.id}
+        editMode={inEditMode}
+        defaultTitle={task.title}
+        dueDate={dueDate}
+        dueTime={dueTime}
+        className="mb-3"
+        onUpdate={(updatedTask) =>
+          updateTaskList({ type: "PUT", task: updatedTask })
+        }
+      />
+    );
+  });
+  console.log(tasksList);
   return (
     <>
-      <Offcanvas show={show} onHide={handleClose} {...props}>
-        <Form noValidate validated={editValidated} onSubmit={onEditFinish}>
-          <Offcanvas.Header>
-            <Offcanvas.Title>
-              {isEditing ? "Edit Tasks" : "Tasks"}
-            </Offcanvas.Title>
-            <div>
-              {isEditing ? (
-                <>
-                  <BackButton type="submit" />
-                  <DeleteAllButton className="me-3" />
-                </>
-              ) : (
-                <>
-                  <AddButton onClick={() => setAddModalShowing(true)} />
-                  <EditButton className="me-3" onClick={toggleEditing} />
-                </>
-              )}
-              <CloseButton className="me-1" onClick={handleClose} />
-            </div>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            {tasksList.map((task) => (
-              <Task
-                key={task.key}
-                editMode={isEditing}
-                defaultTitle={task.title}
-                due={task.dueDateTime}
-                className="mb-3"
-                onUpdate={(updatedTask) =>
-                  updateTaskList({ type: "PUT", task: updatedTask })
-                }
-              />
-            ))}
-          </Offcanvas.Body>
-        </Form>
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        {...props}
+        onExited={() => setInEditMode(false)}
+      >
+        <Offcanvas.Header>
+          <Offcanvas.Title>
+            {inEditMode ? "Edit Tasks" : "Tasks"}
+          </Offcanvas.Title>
+          <div>
+            {inEditMode ? (
+              <>
+                <EditButton onClick={() => setInEditMode(false)} offVariant />
+                <DeleteAllButton className="me-3" />
+              </>
+            ) : (
+              <>
+                <AddButton onClick={() => setAddModalShowing(true)} />
+                <EditButton
+                  className="me-3"
+                  onClick={() => setInEditMode(true)}
+                />
+              </>
+            )}
+            <CloseButton className="me-1" onClick={handleClose} />
+          </div>
+        </Offcanvas.Header>
+        <Offcanvas.Body>{allTasks}</Offcanvas.Body>
       </Offcanvas>
       <AddTaskModal
         show={addModalShowing}
--- src/components/task_panel/Task.jsx
+++ src/components/task_panel/Task.jsx
@@ -1,87 +1,151 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useReducer, useState } from "react";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import DeleteButton from "./DeleteButton";
-import EditButton from "./EditButton";
-import { MdCalendarToday, MdAlarm } from "react-icons/md";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { MdAlarm, MdCalendarToday } from "react-icons/md";
+import DeleteButton from "./DeleteButton";
+import DoneButton from "./DoneButton";
+import EditButton from "./EditButton";
 import dayjs from "dayjs";
 
-function Task({ editMode, ...props }) {
+function Task({ editMode, onUpdate, ...props }) {
   const [title, setTitle] = useState(props.defaultTitle);
   const [isDone, toggleIsDone] = useReducer(
     (currentState) => !currentState,
     false
   );
-  const [date, setDate] = useState(props.due.format("YYYY-MM-DD"));
-  const [time, setTime] = useState(props.due.format("HH:mm"));
-
-  useEffect(() => {
-    if (!editMode) {
-      props.onUpdate({ title, dueDateTime: dayjs(`${date} ${time}`) });
+  const [date, setDate] = useState(props.dueDate);
+  const [time, setTime] = useState(props.dueTime);
+  const [isEditing, toggleEditing] = useReducer(
+    (currentState) => !currentState,
+    false
+  );
+  const [editValidated, setEditValidated] = useState(false);
+  const onEditFinish = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    setEditValidated(true);
+    if (form.checkValidity() !== false) {
+      setEditValidated(false);
+      onUpdate({
+        id: props.id,
+        title,
+        dueDateTime: dayjs(`${date} ${time}`),
+      });
+      toggleEditing();
     }
-  }, [editMode]);
+  };
 
+  if (!editMode) {
+    if (editValidated) setEditValidated(false);
+    if (title !== props.defaultTitle) setTitle(props.defaultTitle);
+    if (date !== props.dueDate) setDate(props.dueDate);
+    if (time !== props.dueTime) setTime(props.dueTime);
+    if (isEditing) toggleEditing();
+  }
   return (
     <Container
       fluid
       className={`d-flex justify-content-between align-items-center ${props.className}`}
     >
       {editMode ? (
-        <>
-          <div>
-            <Form.Group controlId="formTitle">
+        <Form
+          className="px-2"
+          noValidate
+          validated={editValidated}
+          onSubmit={onEditFinish}
+        >
+          <Row>
+            <Form.Group controlId="formTitle" as={Col}>
               <Form.Control
                 type="text"
                 value={title}
                 required
                 onChange={(newValue) => setTitle(newValue.target.value)}
                 placeholder="Enter task name"
+                disabled={!isEditing}
               />
               <Form.Control.Feedback type="invalid">
                 Please enter a name for the task.
               </Form.Control.Feedback>
             </Form.Group>
+            <Col xs={2}>
+              {isEditing ? (
+                <DoneButton type="submit" />
+              ) : (
+                <EditButton onClick={toggleEditing} />
+              )}
+            </Col>
+          </Row>
 
-            <Row className="mt-2">
-              <Form.Group controlId="formDueDate" as={Col}>
-                <Form.Control
-                  type="date"
-                  value={date}
-                  onChange={(newValue) => {
-                    setDate(newValue.target.value);
-                  }}
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a valid due date for the task.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="formDueTime" as={Col}>
-                <Form.Control
-                  type="time"
-                  value={time}
-                  onChange={(newValue) => {
-                    setTime(newValue.target.value);
-                  }}
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a valid due time for the task.
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-          </div>
-          <DeleteButton />
-        </>
+          <Row className="mt-2">
+            <Form.Group controlId="formDueDate" as={Col}>
+              <Form.Control
+                type="date"
+                value={date}
+                onChange={(newValue) => {
+                  setDate(newValue.target.value);
+                }}
+                disabled={!isEditing}
+                max="9999-12-31"
+                min="2023-01-01"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a valid due date for the task.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="formDueTime" as={Col}>
+              <Form.Control
+                type="time"
+                value={time}
+                onChange={(newValue) => {
+                  setTime(newValue.target.value);
+                }}
+                disabled={!isEditing}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a valid due time for the task.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Col xs={2}>
+              <DeleteButton />
+            </Col>
+          </Row>
+        </Form>
       ) : (
         <>
           <div>
-            <h5>{title}</h5>
+            <h5
+              className={
+                isDone ? `text-muted text-decoration-line-through` : ``
+              }
+            >
+              {title}
+            </h5>
             <div className="d-flex align-items-center">
-              <MdCalendarToday className="me-1" />
-              <span className="me-3">{date}</span>
-              <MdAlarm className="me-1" />
-              <span>{time}</span>
+              <MdCalendarToday
+                className="me-1"
+                color={isDone ? "#6c757d" : ""}
+              />
+              <span
+                className={
+                  isDone
+                    ? `text-muted text-decoration-line-through me-3`
+                    : `me-3`
+                }
+              >
+                {date ? date : "No date"}
+              </span>
+              <MdAlarm className="me-1" color={isDone ? "#6c757d" : ""} />
+              <span
+                className={
+                  isDone ? `text-muted text-decoration-line-through` : ``
+                }
+              >
+                {time ? time : "No time"}
+              </span>
             </div>
           </div>
           <input
