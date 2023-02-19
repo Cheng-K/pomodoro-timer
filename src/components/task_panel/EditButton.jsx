ebaa719812ab569ac1bca24acea2b8ed14552d40
Cheng-K <adamkei2612@gmail.com>
2023-02-19 22:49:21 +0800

    Added off variant

--- src/components/task_panel/EditButton.jsx
+++ src/components/task_panel/EditButton.jsx
@@ -1,9 +1,13 @@
-import { Button } from "react-bootstrap";
-import { MdEdit } from "react-icons/md";
-function EditButton({ className = "", iconStyle = "", onClick }) {
+import Button from "react-bootstrap/Button";
+import { MdEdit, MdEditOff } from "react-icons/md";
+function EditButton({ onClick, offVariant = false }) {
   return (
     <Button onClick={onClick} variant="secondary">
-      <MdEdit size="2rem" className="icon-black" />
+      {offVariant ? (
+        <MdEditOff size="2rem" className="icon-black" />
+      ) : (
+        <MdEdit size="2rem" className="icon-black" />
+      )}
     </Button>
   );
 }
