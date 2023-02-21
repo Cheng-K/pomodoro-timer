d23e532f0c778de1d637472c86b4682afb5be13e
Cheng-K <adamkei2612@gmail.com>
2023-02-21 20:43:29 +0800

    Created database and added CRUD functions for tasks

--- src/utilities/database.js
+++ src/utilities/database.js
@@ -1,55 +1,83 @@
-import { openDB, deleteDB, wrap, unwrap } from "idb";
+import Dexie from "dexie";
+import dayjs from "dayjs";
 
-// Schema :
-// Key
-// Title
-// Done
-// Due datetime
+export const db = new Dexie("PomodoroDatabase");
 
-async function openDatabase(name) {
-  return openDB(name);
+db.version(1).stores({
+  tasks: "++id, title, dueDateTime",
+  settings: "name, value",
+});
+
+export async function addTask(newTask) {
+  return db.tasks.add(newTask);
 }
 
-async function initDatabase() {
-  return openDB("master-db", 1, {
-    upgrade(db, oldVersion, newVersion, transaction, event) {
-      db.createObjectStore("tasks", { autoIncrement: true });
-      db.createObjectStore("keyval-settings");
-      transaction
-        .objectStore("tasks")
-        .createIndex("due", "due", { unique: false });
-    },
-  });
+export async function updateTask(newTask) {
+  return db.tasks.put(newTask);
 }
 
-async function insertTask(db, task) {
-  const tx = db.transaction("tasks", "readwrite");
-  const taskStore = tx.objectStore("tasks");
-  await taskStore.add(task);
-  return tx.done;
+export async function removeTask(taskId) {
+  return db.tasks.delete(taskId);
 }
 
-async function updateTask(db, newTask, key) {
-  const tx = db.transaction("tasks", "readwrite");
-  const taskStore = tx.objectStore("tasks");
-  await taskStore.put(newTask, key);
-  return tx.done;
+export function getAllTask() {
+  return db.tasks.toArray();
 }
 
-async function getTask(db, key) {
-  const tx = db.transaction("tasks", "readonly");
-  const taskStore = tx.objectStore("tasks");
-  const result = await taskStore.get(key);
-  await tx.done;
-  return result;
+export async function getAllNotDoneTask() {
+  return db.tasks
+    .filter((t) => !t.done)
+    .toArray()
+    .then((array) => {
+      return array.sort((taskA, taskB) => {
+        const datetimeA = dayjs(taskA.dueDateTime);
+        const datetimeB = dayjs(taskB.dueDateTime);
+        if (!datetimeA.isValid() && !datetimeB.isValid())
+          return 1 ? taskA.title > taskB.title : -1;
+        else if (!datetimeA.isValid()) return 1;
+        else if (!datetimeB.isValid()) return -1;
+        else if (datetimeA.isBefore(datetimeB, "minute")) return -1;
+        else if (datetimeA.isAfter(datetimeB, "minute")) {
+          return 1;
+        } else
+          return 1
+            ? taskA.title > taskB.title
+            : -1
+            ? taskA.title < taskB.title
+            : 0;
+      });
+    });
 }
 
-async function getAllTask(db) {
-  const tx = db.transaction("tasks", "readonly");
-  const taskStore = tx.objectStore("tasks");
-  const result = await taskStore.getAll();
-  await tx.done;
-  return result;
+export async function getAllDoneTask() {
+  return db.tasks
+    .filter((t) => t.done)
+    .toArray()
+    .then((array) => {
+      return array.sort((taskA, taskB) => {
+        const datetimeA = dayjs(taskA.dueDateTime);
+        const datetimeB = dayjs(taskB.dueDateTime);
+        if (!datetimeA.isValid() && !datetimeB.isValid())
+          return 1 ? taskA.title > taskB.title : -1;
+        else if (!datetimeA.isValid()) return 1;
+        else if (!datetimeB.isValid()) return -1;
+        else if (datetimeA.isBefore(datetimeB, "minute")) return 1;
+        else if (datetimeA.isAfter(datetimeB, "minute")) {
+          return -1;
+        } else
+          return 1
+            ? taskA.title > taskB.title
+            : -1
+            ? taskA.title < taskB.title
+            : 0;
+      });
+    });
 }
 
-export { openDatabase, initDatabase };
+export function createTaskForUpdate(id, title, dateObj, done, displayTime) {
+  return { id, title, dueDateTime: dateObj, done, displayTime };
+}
+
+export function createTaskForAdd(title, dateObj, displayTime) {
+  return { title, dueDateTime: dateObj, done: false, displayTime };
+}
