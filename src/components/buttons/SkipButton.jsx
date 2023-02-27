94d90fbe93a40a34759df411658a34f89541a29e
Cheng-K <adamkei2612@gmail.com>
2023-02-27 22:30:25 +0800

    Update component to accept data-cy attribute

--- src/components/buttons/SkipButton.jsx
+++ src/components/buttons/SkipButton.jsx
@@ -2,12 +2,13 @@ import Button from "react-bootstrap/Button";
 import React from "react";
 import { MdSkipNext } from "react-icons/md";
 
-function SkipButton({ className = "", onClick }) {
+function SkipButton({ className = "", onClick, ...props }) {
   return (
     <Button
       variant="secondary"
       className={`rounded-circle control-button d-flex justify-content-center align-items-center ${className}`}
       onClick={onClick}
+      {...props}
     >
       <MdSkipNext className="icon" />
     </Button>
