460722317537f8427403afec74345f5f634a096e
Cheng-K <adamkei2612@gmail.com>
2023-02-27 17:51:24 +0800

    Removed demo spec from cypress

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-describe("My First Test", () => {
-  it("Visits the Kitchen Sink", () => {
-    cy.visit("https://example.cypress.io");
-    cy.contains("type").click();
-    cy.url().should("include", "/commands/actions");
-  });
-});
