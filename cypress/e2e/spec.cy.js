4d721482d3629609f573eb9b704939be15425cbe
Cheng-K <adamkei2612@gmail.com>
2023-02-27 15:10:49 +0800

    Installed and initialized cypress for testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+describe("My First Test", () => {
+  it("Visits the Kitchen Sink", () => {
+    cy.visit("https://example.cypress.io");
+    cy.contains("type").click();
+    cy.url().should("include", "/commands/actions");
+  });
+});
